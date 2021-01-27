28d512e84d41e7c02ac392ef3aed958aed89e5ab
samueldear <samuel_dear@hotmail.com>
2021-01-27 16:54:24 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
